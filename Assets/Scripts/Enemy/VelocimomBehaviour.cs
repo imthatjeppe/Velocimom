7c8a771eb2a875f80676750f8b552e49ae6ab9f4
ljusica <jessiicaceder@hotmail.com>
2021-11-25 13:32:57 +0100

    some changes

--- Assets/Scripts/Enemy/VelocimomBehaviour.cs
+++ Assets/Scripts/Enemy/VelocimomBehaviour.cs
@@ -7,39 +7,45 @@ public class VelocimomBehaviour : MonoBehaviour
     public int speed = 3;
 
     public float startWaitTime;
+    public float distance = 0.2f;
 
     public Transform[] moveSpots;
 
-    private int randomSpot;
+    private int randomDestinationSpot;
 
     private float waitTime;
+
+    private Vector2 deBugPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         waitTime = startWaitTime;
-        randomSpot = Random.Range(0, moveSpots.Length);
+        randomDestinationSpot = Random.Range(0, moveSpots.Length);
+
+        deBugPosition = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Random.Range(0 + 200f, Screen.height - 200f)));
     }
 
     // Update is called once per frame
     void Update()
     {
         Patrol();
-
         //TODO: Chase-script inkl. isHidden
     }
 
     void Patrol()
     {
-        //TODO: Ta hänsyn till väggar/items
-        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        RaycastHit2D sightHit = Physics2D.Raycast(transform.position, transform.right, distance);
 
-        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomDestinationSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[randomDestinationSpot].position) < distance)
         {
+
             if (waitTime <= 0)
             {
                 waitTime = startWaitTime;
-                randomSpot = Random.Range(0, moveSpots.Length);
+                randomDestinationSpot = Random.Range(0, moveSpots.Length);
             }
 
             else
