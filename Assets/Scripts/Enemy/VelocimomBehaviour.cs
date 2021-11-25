02647ff8bfb7283135861294afca2a005e693060
ljusica <jessiicaceder@hotmail.com>
2021-11-25 15:12:31 +0100

    better (but not at all great) pathfinding

--- Assets/Scripts/Enemy/VelocimomBehaviour.cs
+++ Assets/Scripts/Enemy/VelocimomBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class VelocimomBehaviour : MonoBehaviour
 {
@@ -10,12 +11,10 @@ public class VelocimomBehaviour : MonoBehaviour
     public float distance = 0.2f;
 
     public Transform[] moveSpots;
-
+    AIDestinationSetter setDestination;
     private int randomDestinationSpot;
 
     private float waitTime;
-
-    private Vector2 deBugPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +22,9 @@ public class VelocimomBehaviour : MonoBehaviour
         waitTime = startWaitTime;
         randomDestinationSpot = Random.Range(0, moveSpots.Length);
 
-        deBugPosition = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Random.Range(0 + 200f, Screen.height - 200f)));
+        setDestination = GetComponent<AIDestinationSetter>();
+
+        setDestination.target = moveSpots[randomDestinationSpot];
     }
 
     // Update is called once per frame
@@ -35,17 +36,13 @@ public class VelocimomBehaviour : MonoBehaviour
 
     void Patrol()
     {
-        RaycastHit2D sightHit = Physics2D.Raycast(transform.position, transform.right, distance);
-
-        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomDestinationSpot].position, speed * Time.deltaTime);
-
         if (Vector2.Distance(transform.position, moveSpots[randomDestinationSpot].position) < distance)
         {
-
             if (waitTime <= 0)
             {
                 waitTime = startWaitTime;
                 randomDestinationSpot = Random.Range(0, moveSpots.Length);
+                setDestination.target = moveSpots[randomDestinationSpot];
             }
 
             else
