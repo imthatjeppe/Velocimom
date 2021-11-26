9c4cfc2644937e58bc7d41cf16ed84a31ee9d9cf
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2021-11-26 09:25:55 +0100

    Fixed

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,7 +10,11 @@ public class PlayerMovement : MonoBehaviour
     public float AbilityStamina = 100;
     public Slider Staminabar;
     public float currentstamina;
-    private float MinStamina = 0;
+    
+
+    
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,33 +24,34 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
+         float x = Input.GetAxisRaw("Horizontal");
+         float y = Input.GetAxisRaw("Vertical");
 
-        Vector3 movement = new Vector3(x, y).normalized * Time.deltaTime * speed;
+         Vector3 movement = new Vector3(x, y).normalized * Time.deltaTime * speed;
 
-        transform.Translate(movement);
+         transform.Translate(movement);
 
-        /* if (Input.GetKey(KeyCode.W))
-         {
-             transform.Translate(Vector3.up * speed * Time.deltaTime);
-         }
+         /* if (Input.GetKey(KeyCode.W))
+          {
+              transform.Translate(Vector3.up * speed * Time.deltaTime);
+          }
 
-         if (Input.GetKey(KeyCode.A))
-         {
-             transform.Translate(Vector3.left * speed * Time.deltaTime);
-         }
+          if (Input.GetKey(KeyCode.A))
+          {
+              transform.Translate(Vector3.left * speed * Time.deltaTime);
+          }
 
-         if (Input.GetKey(KeyCode.S))
-         {
-             transform.Translate(Vector3.down * speed * Time.deltaTime);
-         }
+          if (Input.GetKey(KeyCode.S))
+          {
+              transform.Translate(Vector3.down * speed * Time.deltaTime);
+          }
 
-         if (Input.GetKey(KeyCode.D))
-         {
-             transform.Translate(Vector3.right * speed * Time.deltaTime);
-         }
-         */
+          if (Input.GetKey(KeyCode.D))
+          {
+              transform.Translate(Vector3.right * speed * Time.deltaTime);
+          }
+          */
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             speed = 0;
