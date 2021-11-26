a67fe82d390b00d3f9b30d1ad53020a1d97abf7d
ljusica <jessiicaceder@hotmail.com>
2021-11-26 09:37:47 +0100

    THE AI PATROL FUCKING WORKS GUYS!!

--- Assets/AstarPathfindingProject/Core/AI/AIPath.cs
+++ Assets/AstarPathfindingProject/Core/AI/AIPath.cs
@@ -99,7 +99,7 @@ namespace Pathfinding {
 		/// <tr><td>[Open online documentation to see videos]</td><td>\xmlonly <verbatim><span class="label label-danger">Too high</span><br/></verbatim>\endxmlonly A too high value will make the agent follow the path too loosely and may cause it to try to move through obstacles.</td></tr>
 		/// </table>
 		/// </summary>
-		public float pickNextWaypointDist = 2;
+		public float pickNextWaypointDist = 0.2f;
 
 		/// <summary>
 		/// Distance to the end point to consider the end of path to be reached.
