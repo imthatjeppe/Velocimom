b41950b9e39c1d4e7f6f90b3884d87a2ccb4b20e
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2021-12-01 15:46:53 +0100

    Update wähhh

--- Assets/Scripts/Player/PlayerDecption.cs
+++ Assets/Scripts/Player/PlayerDecption.cs
@@ -1,34 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class PlayerDecption : MonoBehaviour
 {
-    private bool WaterTap = false;
+    public bool waterTap;
+    public Transform[] moveSpots;
+    private VelocimomBehaviour velocimom;
+    private AIDestinationSetter setDestination;
     // Start is called before the first frame update
     void Start()
     {
-        
+        waterTap = false;
+        velocimom = GameObject.FindGameObjectWithTag("Enemy").GetComponent<VelocimomBehaviour>();
+        setDestination = GameObject.FindGameObjectWithTag("Enemy").GetComponent<AIDestinationSetter>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (waterTap)
+        {
+            velocimom.patrol = false;
+            setDestination.target = moveSpots[0];
+
+           if(velocimom.transform.position == moveSpots[0].position)
+            {
+                waterTap = false;
+                velocimom.patrol = true;
+            }
+        }
         
     }
 
 
     private void TurnOn()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            WaterTap = true;
+ 
+            Debug.Log("Hello");
             
-        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        TurnOn();
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TurnOn();
+            waterTap = true;
+        }
     }
 }
