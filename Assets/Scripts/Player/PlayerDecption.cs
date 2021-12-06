dded02286976fbd5888a507f6e622b4258eaa5af
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2021-12-06 13:00:57 +0100

    rattmorsan står stilla när men använder en deception

--- Assets/Scripts/Player/PlayerDecption.cs
+++ Assets/Scripts/Player/PlayerDecption.cs
@@ -7,54 +7,72 @@ public class PlayerDecption : MonoBehaviour
 {
     public bool enemyLure;
     public Transform[] moveSpotsDeception;
+    public GameObject stopEnemy;
 
     private VelocimomBehaviour velocimom;
     private AIDestinationSetter setDestination;
+    private AIPath AIPath;
     // Start is called before the first frame update
     void Start()
     {
         enemyLure = false;
         velocimom = GameObject.FindGameObjectWithTag("Enemy").GetComponent<VelocimomBehaviour>();
         setDestination = GameObject.FindGameObjectWithTag("Enemy").GetComponent<AIDestinationSetter>();
+        AIPath = stopEnemy.GetComponent<AIPath>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        CheckIfCloseToLure();
+    }
+    public void Resume()
+    {
+        
+        enemyLure = false;
+        velocimom.patrol = true;
+        setDestination.target = velocimom.moveSpots[velocimom.randomDestinationSpot];
+    }
+
+    void CheckIfCloseToLure()
+    {
         if (enemyLure)
         {
-            Invoke(nameof(Luring), 3);
-            velocimom.patrol = false;
-
             if (Vector3.Distance(velocimom.transform.position, moveSpotsDeception[0].position) < 0.2f)
             {
                 Invoke(nameof(Resume), 1);
             }
         }
     }
-    public void Resume()
+
+    void startLure()
     {
-        enemyLure = false;
-        velocimom.patrol = true;
-        setDestination.target = velocimom.moveSpots[velocimom.randomDestinationSpot];
+        if (!enemyLure)
+        {
+            velocimom.patrol = true;
+        }
+        else
+        {
+            AIPath.maxSpeed = 0;
+            Invoke(nameof(Luring), 3);
+        }
     }
     void Luring()
     {
         setDestination.target = moveSpotsDeception[0];
+        AIPath.maxSpeed = 1;
     }
     private void TurnOn()
     {
         Debug.Log("Hello");
     }
-
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (Input.GetKeyDown(KeyCode.E) && velocimom.patrol)
         {
             TurnOn();
             enemyLure = true;
+            startLure();
         }
-
-
     }
 }
