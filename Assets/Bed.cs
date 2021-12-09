f866e82e0a563eca569f79b2cb66bd3f6f75a045
Sappels <sappels66@gmail.com>
2021-12-09 10:57:08 +0100

    Creating bed script

---
+++ Assets/Bed.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed : MonoBehaviour
+{
+    public GameObject dropOffZone;
+    private Score scoreRef;
+
+
+    private void Start()
+    {
+       scoreRef = dropOffZone.GetComponent<Score>();
+
+        //Hämta score component
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKey("D"))
+        {
+            if (scoreRef.score > /*scoreRef.scoreNeededToWin*/)
+            {
+                //Ladda scen
+            }
+        }
+    }
+}
