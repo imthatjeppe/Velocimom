b20a97f2716bc4e50064736a34ef24c2666dbb0a
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2021-12-14 10:03:44 +0100

    Spelaren droppar allt på samma gång

--- Assets/Scripts/Test/Viktors lekhörna/PlayerManager.cs
+++ Assets/Scripts/Test/Viktors lekhörna/PlayerManager.cs
@@ -8,6 +8,8 @@ public class PlayerManager : MonoBehaviour
     public InteractionFood currentInterObjScript = null;
     public Inventory inventory;
     public bool canNotDie;
+
+    private bool inDropZone;
     
     private void Update()
     {
@@ -22,19 +24,42 @@ public class PlayerManager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            inventory.DropItem();
+            if (inDropZone)
+            {
+                dropAllItems();
+            }
+            else
+            {
+                inventory.DropItem();
+            }
         }
+
+        
+        
+    }
+
+
+    private void dropAllItems()
+    {
+            for (int i = 0; i < inventory.inventoryCount; inventory.inventoryCount--)
+            {
+                inventory.DropItem();
+            }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Food"))
         {
-            currentInterObj = other.gameObject;
-            currentInterObjScript = currentInterObj.GetComponent<InteractionFood>();
+                currentInterObj = other.gameObject;
+                currentInterObjScript = currentInterObj.GetComponent<InteractionFood>();
+        }
+
+        if (other.CompareTag("DropOff"))
+        {
+            inDropZone = true;
         }
     }
-
     private void OnTriggerExit2D(Collider2D other)
     {
         if(other.CompareTag("Food"))
@@ -43,6 +68,11 @@ public class PlayerManager : MonoBehaviour
             {
                 currentInterObj = null;
             }
+        }
+        
+        if (other.CompareTag("DropOff"))
+        {
+            inDropZone = false;
         }
     }
 }
