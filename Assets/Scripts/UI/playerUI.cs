bd404055175bc3822104546c3a9179acb5d7023e
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2021-12-30 15:11:28 +0100

    Star system is fixed and also the PlayerUI is finished

---
+++ Assets/Scripts/UI/playerUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerUI : MonoBehaviour
+{
+
+    public GameObject clockObject;
+    public Slider clockSlider;
+
+    private Timer timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = GameObject.FindGameObjectWithTag("DropOff").GetComponent<Timer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clockSlider.value = timer.GetTimer();
+    }
+}
